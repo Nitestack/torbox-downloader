917771f7d69eab2f6c7db55a3ac6e8dacb651cc8
Nitestack <74626967+Nitestack@users.noreply.github.com>
2025-01-10 22:58:11 +0100

    init: upload progress

---
+++ options/script.js
@@ -0,0 +1,26 @@
+const ids = [
+  "torboxApiKey",
+  "discordGamesWebhookURL",
+  "discordOthersWebhookURL",
+];
+
+browser.storage.local.get(ids).then((result) => {
+  for (const [id, value] of Object.entries(result)) {
+    document.getElementById(id).defaultValue = value;
+  }
+});
+
+document.getElementById("options-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  /** @type {Record<string, string>} */
+  const newStorage = {};
+
+  for (const id of ids) {
+    newStorage[id] = formData.get(id) ?? null;
+  }
+
+  browser.storage.local.set(newStorage);
+});
